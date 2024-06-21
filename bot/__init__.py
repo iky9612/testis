3a86c5f763bc86901899d78439a8eee343f2ae97
iky9612 <149653006+iky9612@users.noreply.github.com>
2024-06-22 06:43:02 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -99,7 +99,7 @@ if len(DATABASE_URL) == 0:
 if DATABASE_URL:
     try:
         conn = MongoClient(DATABASE_URL, server_api=ServerApi("1"))
-        db = conn.mltb
+        db = conn.mltbxyz
         current_config = dict(dotenv_values("config.env"))
         old_config = db.settings.deployConfig.find_one({"_id": bot_id})
         if old_config is None:
